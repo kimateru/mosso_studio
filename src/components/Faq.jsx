b4b00b790a00c471fcba82f25279c0086b2ecd6e
kimateru <danea0702@gmail.com>
2024-07-16 20:48:44 +0300

    fixed dropdown,added visual chaining elements,minor design improvements

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -15,18 +15,18 @@ const Faq = () => {
         <div className='w-full'>
           <button
             onClick={() => toggleDropdown(0)}
-            className='rounded-xl w-full bg-[#747373] text-white py-2 px-2 text-sm flex items-center justify-center gap-2'
+            className='rounded-full w-full bg-[#747373] text-white py-2 px-2 text-sm flex items-center justify-center gap-2'
           >
             <span>Дизайн - проект</span> <FaCaretDown />
           </button>
           {openDropdown === 0 && (
-            <div className="bg-black text-white p-4 mt-2 rounded-xl transition-all duration-300 text-sm flex flex-col gap-4">
-              <p>01) Сбор информации,обсуждение предпочтений, бюджета и сроков.</p>
-              <p>02) Исследование объекта: Замеры, фотографии, изучение технических характеристик помещения.</p>
-              <p>03) Создание планировочных решений и размещение мебели.</p>
-              <p>04) Дизайн всех помещений в фотореалестичных 3Д визуализациях.</p>
-              <p>05) Полный пакет чертежей для строителей, чертежи мебели индивидуальной разработки.</p>
-              <p>06) Печатная версия проекта в 2х экземплярах.</p>
+            <div className="text-black p-2 pl-4 mt-2 rounded-xl transition-all duration-300 text-sm flex flex-col gap-4 text-left">
+              <p className='flex gap-2 border-b pb-2'><span className='text-sm'>01.</span> Сбор информации,обсуждение предпочтений, бюджета и сроков.</p>
+              <p className='flex gap-2 border-b pb-2'><span className='text-sm'>02.</span> Исследование объекта: Замеры, фотографии, изучение технических характеристик помещения.</p>
+              <p className='flex gap-2 border-b pb-2'><span className='text-sm'>03.</span> Создание планировочных решений и размещение мебели.</p>
+              <p className='flex gap-2 border-b pb-2'><span className='text-sm'>04.</span> Дизайн всех помещений в фотореалестичных 3Д визуализациях.</p>
+              <p className='flex gap-2 border-b pb-2'><span className='text-sm'>05.</span> Полный пакет чертежей для строителей, чертежи мебели индивидуальной разработки.</p>
+              <p className='flex gap-2 border-b pb-2'><span className='text-sm'>06.</span> Печатная версия проекта в 2х экземплярах.</p>
             </div>
           )}
         </div>
@@ -34,18 +34,18 @@ const Faq = () => {
         <div className='w-full'>
           <button
             onClick={() => toggleDropdown(1)}
-            className='rounded-xl w-full bg-[#747373] text-white py-2 px-2 text-sm flex items-center justify-center gap-2'
+            className='rounded-full w-full bg-[#747373] text-white py-2 px-2 text-sm flex items-center justify-center gap-2'
           >
             <span>Авторское сопровождение</span> <FaCaretDown />
           </button>
           {openDropdown === 1 && (
-            <div className="bg-black text-white p-4 mt-2 rounded-xl transition-all duration-300 text-sm flex flex-col gap-4">
-              <p>01) Подбор строительной бригады </p>
-              <p>02) Проверка выполнения строительных и отделочных работ в соответствии с проектной документацией.</p>
-              <p>03) Контроль качества используемых материалов и работ.</p>
-              <p>04) Внесение необходимых изменений в проектную документацию по ходу работ</p>
-              <p>05) Регулярные визиты на объект для проверки этапов работ,финальная приемка выполненных работ и проверка на соответствие проекту.</p>
-              <p>06) Оперативное реагирование на любые изменения или непредвиденные ситуации на объекте.</p>
+            <div className="text-black p-2 pl-4 mt-2 rounded-xl transition-all duration-300 text-sm flex flex-col gap-4 text-left">
+              <p className='flex gap-2 border-b pb-2'><span className='text-sm'>01.</span>Подбор строительной бригады </p>
+              <p className='flex gap-2 border-b pb-2'><span className='text-sm'>02.</span>Проверка выполнения строительных и отделочных работ в соответствии с проектной документацией.</p>
+              <p className='flex gap-2 border-b pb-2'><span className='text-sm'>03.</span>Контроль качества используемых материалов и работ.</p>
+              <p className='flex gap-2 border-b pb-2'><span className='text-sm'>04.</span>Внесение необходимых изменений в проектную документацию по ходу работ</p>
+              <p className='flex gap-2 border-b pb-2'><span className='text-sm'>05.</span>Регулярные визиты на объект для проверки этапов работ,финальная приемка выполненных работ и проверка на соответствие проекту.</p>
+              <p className='flex gap-2 border-b pb-2'><span className='text-sm'>06.</span>Оперативное реагирование на любые изменения или непредвиденные ситуации на объекте.</p>
             </div>
           )}
         </div>
@@ -53,18 +53,18 @@ const Faq = () => {
         <div className='w-full'>
           <button
             onClick={() => toggleDropdown(2)}
-            className='rounded-xl w-full bg-[#747373] text-white py-2 px-2 text-sm flex items-center justify-center gap-2'
+            className='rounded-full w-full bg-[#747373] text-white py-2 px-2 text-sm flex items-center justify-center gap-2'
           >
             <span>Комплектация</span> <FaCaretDown />
           </button>
           {openDropdown === 2 && (
-            <div className="bg-black text-white p-4 mt-2 rounded-xl transition-all duration-300 text-sm flex flex-col gap-4 ">
-              <p>01) Организация процесса закупки </p>
-              <p>02) Размещение заказов у поставщиков,организация доставки материалов и мебели на объект</p>
-              <p>03) Подбор и согласование образцов отделочных материалов, мебели и декора.</p>
-              <p>04) Организация и контроль установки мебели и оборудования.</p>
-              <p>05) Решение возникающих задач в процессе реализации </p>
-              <p>06) Окончательная проверка всех позиций на соответствие проекту.</p>
+            <div className="text-black p-2 pl-4 mt-2 rounded-xl transition-all duration-300 text-sm flex flex-col gap-4 text-left">
+              <p className='flex gap-2 border-b pb-2'><span className='text-sm'>01.</span>Организация процесса закупки </p>
+              <p className='flex gap-2 border-b pb-2'><span className='text-sm'>02.</span>Размещение заказов у поставщиков,организация доставки материалов и мебели на объект</p>
+              <p className='flex gap-2 border-b pb-2'><span className='text-sm'>03.</span>Подбор и согласование образцов отделочных материалов, мебели и декора.</p>
+              <p className='flex gap-2 border-b pb-2'><span className='text-sm'>04.</span>Организация и контроль установки мебели и оборудования.</p>
+              <p className='flex gap-2 border-b pb-2'><span className='text-sm'>05.</span>Решение возникающих задач в процессе реализации </p>
+              <p className='flex gap-2 border-b pb-2'><span className='text-sm'>06.</span>Окончательная проверка всех позиций на соответствие проекту.</p>
             </div>
           )}
         </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import Logo from '../assets/icons/logo.jpg';
-import ProjA3 from '../assets/img/proj-1.2.jpg';
+import Header1 from '../assets/img/1.jpg';
+import Header2 from '../assets/img/2.jpg';
 import { FaTelegram } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa6";
 import { FaWhatsapp } from "react-icons/fa";
@@ -11,28 +12,35 @@ const Hero = () => {
             <div className='flex flex-col gap-5 justify-between'>
                 <p className='border-t-2 border-b-2 border-black tracking text-sm'>Студия дизайна и 3D</p>
                 <h1 className='font-bold text-xl'>Дизайн, c вниманием к каждой детали.</h1>
-                <p className='text-sm max-w-[80%] mx-auto'>В стремительном ритме современной жизни дом- это то место, где хочется отдохнуть телом и душой. Каждый интерьер индивидуален и уникален, поэтому мы детально изучаем ваши желания и потребности, создавая пространство, идеально гармонирующее с вашим образом жизни.</p>
+                <img src={Header1} alt="project img" className='h-[15%] object-cover mt-4' />
+                <p className='text-sm max-w-[80%] mx-auto text-left'>В стремительном ритме современной жизни дом- это то место, где хочется отдохнуть телом и душой. Каждый интерьер индивидуален и уникален, поэтому мы детально изучаем ваши желания и потребности, создавая пространство, идеально гармонирующее с вашим образом жизни.</p>
             </div>
             <div className='flex flex-col gap-5 justify-between mt-5'>
                 <img src={Logo} alt="Mosso" className='rounded-full w-[150px] mx-auto' />
-                <p className='text-sm max-w-[80%] mx-auto'>Добрый день! Меня зовут Мария,я дизайнер интерьеров ,3D визуализатор и основатель студии MOSSO.</p>
-                <p className='text-sm max-w-[80%] mx-auto'>Мы специализируемся на минималистичных интерьерах, которые обеспечивают максимальный комфорт и эстетическое наслаждение.</p>
+                <p className='text-sm max-w-[80%] mx-auto text-left'>Добрый день! Меня зовут Мария,я дизайнер интерьеров ,3D визуализатор и основатель студии MOSSO.</p>
+                <p className='border-t-2 border-b-2 border-black tracking text-sm'>Философия студии</p>
+                <p className='text-sm max-w-[80%] mx-auto text-left'>
+                Философия нашей студии - превратить вашу мечту о идеальном доме в реальность, делая это с максимальным удобством и без лишнего стресса, чтобы после завершения этого этапа вашей жизни,у вас остались только теплые воспоминания и впечатления.</p>
                 <ul className='grid grid-cols-1 grid-rows-4 gap-4 text-3xl items-start px-4 mt-3 mx-auto sm:grid-cols-2 sm:grid-rows-2'>
                     <a className='flex items-center gap-2' href=""><FaTelegram /> <span className='text-sm'>- @mosso</span></a>
                     <a className='flex items-center gap-2' href=""><FaInstagram /> <span className='text-sm'>- @mosso</span></a>
                     <a className='flex items-center gap-2' href=""><FaWhatsapp /> <span className='text-sm'>- +373 (69) 666999</span></a>
                     <a className='flex items-center gap-2' href=""><FaViber /><span className='text-sm'>- +373 (69) 666999</span></a>
                 </ul>
-                <img src={ProjA3} alt="project img" className='h-[15%] object-cover mt-4' />
+                <div
+                    className='relative h-[500px] w-full bg-cover bg-center bg-fixed mt-4'
+                    style={{ backgroundImage: `url(${Header2})` }}
+                >
+                </div>
             </div>
             <div className='flex flex-col gap-5 justify-between mt-5'>
-                <p className='border-t-2 border-b-2 border-black tracking text-sm'>Mosso</p>
+                <p className='border-t-2 border-b-2 border-black tracking text-sm'>MOSSO</p>
                 <h1 className='font-bold text-xl'>Ваш дом-это ваше отражение!</h1>
-                <p className='text-sm max-w-[80%] mx-auto'>Философия нашей студии - превратить вашу мечту о идеальном доме в реальность, делая это с максимальным удобством и без лишнего стресса, чтобы после завершения этого этапа вашей жизни,у вас остались только теплые воспоминания и впечатления.</p>
+                <p className='text-sm max-w-[80%] mx-auto text-left'>Мы специализируемся на минималистичных интерьерах, которые обеспечивают максимальный комфорт и эстетическое наслаждение.</p>
             </div>
             <div className='w-full h-1 border-b-2 mt-5'></div>
         </section>
-    )
+    );
 }
 
-export default Hero
+export default Hero;
