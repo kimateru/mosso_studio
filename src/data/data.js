a44e4bdb6a9c1535397009780cceb944beddcac4
kimateru <danea0702@gmail.com>
2024-07-22 00:14:33 +0300

    seo optimization for search engine v0.1

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,7 @@
 const data = [
-    { 'elegance classic': ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg','12.jpg', '13.jpg', '14.jpg', '15.jpg']},
-    {'serene minimalism': ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg']},
-    {'soft modern': ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg','12.jpg','13.jpg']},
-    {'whitespace': ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg','11.jpg','12.jpg']},
+    { 'elegance classic': ['1.webp', '2.webp', '3.webp', '4.webp', '5.webp', '6.webp', '7.webp', '8.webp', '9.webp', '10.webp', '11.webp','12.webp', '13.webp', '14.webp', '15.webp']},
+    {'serene minimalism': ['1.webp', '2.webp', '3.webp', '4.webp', '5.webp', '6.webp']},
+    {'soft modern': ['1.webp', '2.webp', '3.webp', '4.webp', '5.webp', '6.webp', '7.webp', '8.webp', '9.webp', '10.webp', '11.webp','12.webp','13.webp']},
+    {'whitespace': ['1.webp', '2.webp', '3.webp', '4.webp', '5.webp', '6.webp', '7.webp', '8.webp', '9.webp', '10.webp','11.webp','12.webp']},
 ]
 export default data
