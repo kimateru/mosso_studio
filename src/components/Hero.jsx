27d4f3b85764f04933e86d545f4378874d067c44
kimateru <danea0702@gmail.com>
2024-07-22 01:01:22 +0300

    seo optimization for search engine v0.6

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,42 +1,33 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import Logo from '/assets/icons/logo.webp';
+import Header1 from '/assets/img/1.webp';
+import Header2 from '/assets/img/2.webp';
 import { FaTelegram, FaInstagram, FaWhatsapp, FaViber } from "react-icons/fa";
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const Hero = () => {
     return (
         <section id='hero' className='flex flex-col mt-4 text-center gap-5'>
+            <Helmet>
+                <link rel="preload" as="image" href={Header1} />
+            </Helmet>
             <div className='flex flex-col gap-5 justify-between animate-fadeIn'>
                 <p className='border-t-2 border-b-2 border-black tracking text-sm animate-slideIn'>Студия дизайна и 3D</p>
                 <h1 className='font-bold text-xl md:text-2xl lg:text-3xl animate-slideIn'>Дизайн, c вниманием к каждой детали.</h1>
-                <LazyLoadImage
-                    src="/assets/img/1.webp"
-                    alt="project img"
-                    effect="blur"
-                    className="h-[15%] object-cover mt-4 md:h-[600px] sm:w-[80%] lg:w-[60%] sm:mx-auto"
-                />
+                <img src={Header1} alt="project img" className='h-[15%] object-cover mt-4 md:h-[600px] sm:w-[80%] lg:w-[60%] sm:mx-auto animate-fadeIn' />
                 <p className='text-sm max-w-[80%] mx-auto text-center lg:max-w-[60%] md:text-base lg:text-lg animate-fadeIn'>
                     В стремительном ритме современной жизни дом- это то место, где хочется отдохнуть телом и душой. Каждый интерьер индивидуален и уникален, поэтому мы детально изучаем ваши желания и потребности, создавая пространство, идеально гармонирующее с вашим образом жизни.
                 </p>
-                <div className='flex justify-center'>
-                    <LazyLoadImage
-                        src="/assets/icons/logo.webp"
-                        alt="Mosso"
-                        effect="blur"
-                        className='rounded-full mx-auto animate-zoomIn'
-                        width={150}
-                        height={150}
-                    />
-                </div>
             </div>
+            <img src={Logo} alt="Mosso" className='rounded-full w-[150px] mx-auto animate-zoomIn' />
             <div className='flex flex-col gap-5 justify-between mt-5 animate-fadeIn' id='contacts'>
                 <p className='text-sm max-w-[80%] mx-auto text-center lg:max-w-[60%] md:text-base lg:text-lg animate-slideIn'>
-                    Добрый день! Меня зовут Мария, я дизайнер интерьеров, 3D визуализатор и основатель студии MOSSO.
+                    Добрый день! Меня зовут Мария,я дизайнер интерьеров ,3D визуализатор и основатель студии MOSSO.
                 </p>
                 <p className='border-t-2 border-b-2 border-black tracking text-sm animate-slideIn'>Философия студии</p>
                 <div className='max-w-[80%] lg:max-w-[60%] mx-auto flex flex-col items-center animate-fadeIn'>
                     <p className='text-sm text-center md:text-base lg:text-lg'>
-                        Философия нашей студии - превратить вашу мечту о идеальном доме в реальность, делая это с максимальным удобством и без лишнего стресса, чтобы после завершения этого этапа вашей жизни, у вас остались только теплые воспоминания и впечатления.
+                        Философия нашей студии - превратить вашу мечту о идеальном доме в реальность, делая это с максимальным удобством и без лишнего стресса, чтобы после завершения этого этапа вашей жизни,у вас остались только теплые воспоминания и впечатления.
                     </p>
                     <ul className='flex flex-col items-center gap-2 text-3xl mt-3 sm:flex-row w-full text-center justify-between'>
                         <li className='w-full flex justify-center'>
@@ -56,7 +47,7 @@ const Hero = () => {
                 <p className='border-t-2 border-b-2 border-black tracking text-sm h-[24px] animate-slideIn'></p>
                 <div
                     className='relative h-[500px] w-full bg-cover bg-bottom animate-fadeIn'
-                    style={{ backgroundImage: `url(/assets/img/2.webp)` }}
+                    style={{ backgroundImage: `url(${Header2})` }}
                 >
                 </div>
             </div>
